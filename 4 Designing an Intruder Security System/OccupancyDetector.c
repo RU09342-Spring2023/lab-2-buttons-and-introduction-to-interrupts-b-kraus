32d6ec45ca1ea71e05b6769750bd2dbab5ac4aca
Beth Kraus <krausb87@students.rowan.edu>
2023-02-12 18:14:18 -0500

    Setup
    This is the setup for my code, it is now ready for me to start

--- 4 Designing an Intruder Security System/OccupancyDetector.c
+++ 4 Designing an Intruder Security System/OccupancyDetector.c
@@ -1,10 +1,34 @@
 /*
  * OccupancyDetector.c
  *
- *  Created on: Jan 30, 2023
- *      Author: russty
+ *  Created on: Feb. 12, 2023
+ *      Author: Beth Kraus
  */
 
+#include <msp430.h>
+
+void gpioInit();
+
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+
+    gpioInit();                 // Initialize all GPIO Pins for the project
 
 
+    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
+                                            // to activate previously configured port settings
 
+
+void gpioInit()
+{
+         P1DIR |= BIT0;             // Configure P1.0 to an Output
+       P6DIR |= BIT6;              // Configure P6.6 to an Output
+       P2DIR &= ~BIT3;             // Configure P2.3 to an Input
+       P4DIR &= ~BIT1;             // Configure P4.1 to an Input
+
+       P2REN |= BIT3;               // Enable Resistor on P2.3
+       P2OUT |= BIT3;               // Configure Resistor on P2.3 to be Pullup
+       P4REN |= BIT1;               // Enable Resistor on P4.1
+       P4OUT |= BIT1;               // Configure Resistor on P4.1 to be Pullup
+}
