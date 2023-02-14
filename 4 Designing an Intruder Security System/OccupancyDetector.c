af1cf365de2683b9a09bd014b4d57995bf1f6f5a
Beth Kraus <krausb87@students.rowan.edu>
2023-02-14 12:09:40 -0500

    Test 1
    This is test of the basic setup of the program, this is not the final code, but more of a test of the switch statement and some of the cases

--- 4 Designing an Intruder Security System/OccupancyDetector.c
+++ 4 Designing an Intruder Security System/OccupancyDetector.c
@@ -6,10 +6,12 @@
  */
 
 #include <msp430.h>
+#include <stdint.h>
+#include <stdbool.h>
 #define ARMED 0
 #define WARNING 1
-#define ALARM
-
+#define ALARM 2
+char ALARM_SYSTEM;
 void gpioInit();
 
 int main(void)
@@ -22,10 +24,34 @@ int main(void)
     PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
                                             // to activate previously configured port settings
 
+  switch(ALARM_SYSTEM) {
+      case ARMED: {
+       while(1)
+       {  P1OUT &= ~BIT0;
+          P6OUT ^= BIT6;
+         __delay_cycles(3000000);
+       }
+          break;
+      }
+      case WARNING: {
+          P1OUT &= ~BIT0;
+          P6OUT ^= BIT6;
+          __delay_cycles(500000);
 
-void gpioInit()
-{
-       P1DIR |= BIT0;             // Configure P1.0 to an Output
+          break;
+    }
+      case ALARM: {
+          if (!(P4IN & BIT1))
+          { P1OUT |= BIT0;}
+          if ((P4IN & BIT1))
+            { P6OUT |= BIT6; }
+
+       break;
+
+      }
+
+void gpioInit();
+     { P1DIR |= BIT0;             // Configure P1.0 to an Output
        P6DIR |= BIT6;              // Configure P6.6 to an Output
        P2DIR &= ~BIT3;             // Configure P2.3 to an Input
        P4DIR &= ~BIT1;             // Configure P4.1 to an Input
@@ -34,4 +60,6 @@ void gpioInit()
        P2OUT |= BIT3;               // Configure Resistor on P2.3 to be Pullup
        P4REN |= BIT1;               // Enable Resistor on P4.1
        P4OUT |= BIT1;               // Configure Resistor on P4.1 to be Pullup
+    }
+  }
 }
