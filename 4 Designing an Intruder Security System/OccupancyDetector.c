99db44792c3f452dac0062ea45520271403af36b
Beth Kraus <krausb87@students.rowan.edu>
2023-02-21 14:54:03 -0500

    Final Code

--- 4 Designing an Intruder Security System/OccupancyDetector.c
+++ 4 Designing an Intruder Security System/OccupancyDetector.c
@@ -21,36 +21,51 @@ int main(void)
     gpioInit();                 // Initialize all GPIO Pins for the project
 
 
-    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
-                                            // to activate previously configured port settings
-
-  switch(ALARM_SYSTEM) {
-      case ARMED: {
-       while(1)
-       {  P1OUT &= ~BIT0;
-          P6OUT ^= BIT6;
-         __delay_cycles(3000000);
-       }
+    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode// to activate previously configured port settings
+  int i =0;
+    while(1)
+    {
+        switch(ALARM_SYSTEM) {
+        case ARMED: {
+          if (!(P2IN & BIT3))
+              ALARM_SYSTEM = WARNING;
+        else
+            { P1OUT &= ~BIT0;
+            P6OUT ^= BIT6;
+         __delay_cycles(3000000);}
           break;
       }
       case WARNING: {
-          P1OUT &= ~BIT0;
-          P6OUT ^= BIT6;
-          __delay_cycles(500000);
+          P1OUT ^= BIT0;
+          P6OUT &= ~BIT6;
+
+          if (i>=20)
+             {ALARM_SYSTEM = ALARM;
+             i = 0;}
+          else if (P2IN & BIT3)
+          {ALARM_SYSTEM = ARMED;}
+          else
+              {i++;
+          __delay_cycles(500000);}
 
           break;
     }
       case ALARM: {
-          if (!(P4IN & BIT1))
+          if ((P4IN & BIT1))//BUTTON PRESSED
           { P1OUT |= BIT0;}
-          if ((P4IN & BIT1))
-            { P6OUT |= BIT6; }
+          else if (!(P4IN & BIT1))//NOT PRESSED
+            { ALARM_SYSTEM = ARMED; }
 
        break;
 
       }
 
-void gpioInit();
+
+        }
+    }
+}
+
+void gpioInit()
      { P1DIR |= BIT0;             // Configure P1.0 to an Output
        P6DIR |= BIT6;              // Configure P6.6 to an Output
        P2DIR &= ~BIT3;             // Configure P2.3 to an Input
@@ -61,5 +76,3 @@ void gpioInit();
        P4REN |= BIT1;               // Enable Resistor on P4.1
        P4OUT |= BIT1;               // Configure Resistor on P4.1 to be Pullup
     }
-  }
-}
